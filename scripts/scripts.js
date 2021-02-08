e11dba327b4dc3de2a739cf661f4b11bc7eb353a
Evgenia Bazelan <evgeniabazelan@gmail.com>
2021-02-08 20:19:38 +0300

    change js

--- scripts/scripts.js
+++ scripts/scripts.js
@@ -3,15 +3,17 @@ let saveBtn = form.querySelector('.form__button');
 let closeBtn = form.querySelector('.form__close');
 let overlay = document.querySelector('.overlay');
 
-let formElements = document.getElementsByClassName('form');
-let overlayElements = document.getElementsByClassName('overlay');
+//let formElements = document.getElementsByClassName('form');
+
+//let overlayElements = document.getElementsByClassName('overlay');
+
 
 let profile = document.querySelector('.profile');
 let editBtn = profile.querySelector('.profile__edit-button');
 let addBtn = profile.querySelector('.profile__add-button');
 let profName = profile.querySelector('.profile__name');
 let profession = profile.querySelector('.profile__profession');
-let likeButtons = document.getElementsByClassName('photo-grid__like');
+let likeButtons = document.querySelector('.photo-grid__like');
 
 let formInfo = document.querySelector('form[name=form]');
 let changeName = formInfo.querySelector('input[name=name]');
@@ -19,17 +21,23 @@ let changeProfession = formInfo.querySelector('input[name=profession');
 
 
 
-function formOpen() {
-    Array.from(overlayElements).forEach(element => element.classList.toggle('overlay_open'));
-    Array.from(formElements).forEach(element => element.classList.toggle('form_open'));
+//function formOpen() {
+// Array.from(overlayElements).forEach(element => element.classList.toggle('overlay_open'));
+// Array.from(formElements).forEach(element => element.classList.toggle('form_open'));
+// changeName.value = profName.textContent;
+//changeProfession.value = profession.textContent;
+//}
+function addClassOverlay() {
+    overlay.classList.add('overlay_open');
     changeName.value = profName.textContent;
     changeProfession.value = profession.textContent;
 }
 
 
 function formClose() {
-    Array.from(overlayElements).forEach(element => element.classList.remove('overlay_open'));
-    Array.from(formElements).forEach(element => element.classList.remove('form_open'));
+    //Array.from(overlayElements).forEach(element => element.classList.remove('overlay_open'));
+    //Array.from(formElements).forEach(element => element.classList.remove('form_open'));
+    overlay.classList.remove('overlay_open');
 }
 
 
@@ -38,16 +46,20 @@ function formFill(e) {
     profName.textContent = changeName.value;
     profession.textContent = changeProfession.value;
     formClose();
-
 }
 
-Array.from(likeButtons).forEach(element => element.addEventListener('click', likeActive));
+//Array.from(likeButtons).forEach(element => element.addEventListener('click', likeActive));
 
 
-function likeActive(e) {
-    e.target.classList.toggle('photo-grid__like_active');
+//function likeActive(e) {
+//  e.target.classList.toggle('photo-grid__like_active');
+//}
+function likeActive() {
+    likeButtons.classList.add('photo-grid__like_active');
 }
 
-editBtn.addEventListener('click', formOpen);
+likeButtons.addEventListener('click', likeActive);
+editBtn.addEventListener('click', addClassOverlay);
+//editBtn.addEventListener('click', formOpen);
 form.addEventListener('submit', formFill);
 closeBtn.addEventListener('click', formClose);
